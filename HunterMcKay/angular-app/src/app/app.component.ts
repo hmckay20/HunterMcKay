07e44fd4268e7f9360669abbacac595cd744dba7
hmckay20 <98140040+hmckay20@users.noreply.github.com>
2023-08-01 00:04:50 -0500

    trying

--- HunterMcKay/angular-app/src/app/app.component.ts
+++ HunterMcKay/angular-app/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'My angular app';
+  title = 'This is my app';
+  showImage = false;
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
 }
